ad6590f7c8868828c063881016758614727d98e7
MinimalistPeach <stremler.lacika77@gmail.com>
2023-03-16 17:37:12 +0100

    making the 7.task

--- First_Lesson/7.task.c
+++ First_Lesson/7.task.c
@@ -1,26 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <xlsxwriter.h>
+#include "xlsxwriter.h"
 
 int main()
 {
 
+    lxw_workbook  *workbook  = workbook_new("7task.xlsx");
+    lxw_worksheet *worksheet = workbook_add_worksheet(workbook, NULL);
+    int row = 1, col = 0;
+    worksheet_write_string(worksheet, 0, col, "run time", NULL);
+    worksheet_write_string(worksheet, 0, col+1, "prime number count", NULL);
+
+    
     int isPrime, max, primenumbers = 0;
 
     printf("Input a number larger than 999: ");
     scanf("%d", &max);
 
-    printf("MAX: %d\n", max);
-
-    lxw_workbook *workbook = workbook_new("7.task.xlsx");
-
-    int row, col = 1;
-    lxw_worksheet *worksheet = workbook_add_worksheet(workbook, NULL);
+    //printf("MAX: %d\n", max);
     clock_t start = clock();
-    for (int i = 0; i < max; i++)
+    int i;
+    for (i = 0; i < max; i++)
     {
-
+        
         if (i > 1)
         {
             isPrime = 1;
@@ -34,18 +37,19 @@ int main()
                 }
             }
         }
-        if (isPrime == 1)
-        {
-            primenumbers++;
-        }
+        if(isPrime) primenumbers++;
         if (i % 1000 == 0)
         {
             clock_t end = clock();
             float seconds = (float)(end - start) / CLOCKS_PER_SEC;
-            printf("\n running time: %f prime number count: %d", seconds, primenumbers);
+            printf("\n run time: %f prime number count: %d", seconds, primenumbers);
+            worksheet_write_number(worksheet, row, col,     seconds, NULL);
             worksheet_write_number(worksheet, row, col + 1, primenumbers, NULL);
-            worksheet_write_number(worksheet, row + 1, col + 1, seconds, NULL);
+            row++;
         }
     }
+
     return workbook_close(workbook);
+
+    
 }
