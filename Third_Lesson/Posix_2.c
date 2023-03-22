1e78005cf108095e6bcb23f8a90565cb13494904
MinimalistPeach <stremler.lacika77@gmail.com>
2023-03-22 13:18:30 +0100

    Making the second Posix task

---
+++ Third_Lesson/Posix_2.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+
+void *secondary_thread(void *vargp)
+{
+    printf("%d.szal elinditva\n", (int*)vargp);
+
+    return NULL;
+}
+
+int main()
+{
+    pthread_t thread_id;
+    int num = 60;
+    int i;
+    printf("%d szal elinditasa\n", num);
+    for (i = 0; i < num; i++)
+    {
+        pthread_create(&thread_id, NULL, secondary_thread, i + 1);
+    }
+
+    printf("\nSzalak lelovese\n");
+    for (i = 0; i < num; i++)
+    {
+        pthread_join(thread_id, NULL);
+    }
+    printf("\nSzalak befejezve! Nyomjon ENTER-t a programbol kilepeshez.");
+    getchar();
+    exit(0);
+}
